b4e04fb0729acb26ca3478c166f5c43dffb8a14a
isuyeon <45350108+isuyeon@users.noreply.github.com>
2018-12-28 18:10:59 +0900

    과제

--- js/userName.js
+++ js/userName.js
@@ -1,13 +1,28 @@
 const userNameContainer = document.querySelector('.user-name');
 const userNameInput = userNameContainer.querySelector('input');
+const userGreeting = document.querySelector('.user-greeting');
 const userName = document.querySelector('.user-text');
+const btnReset = userGreeting.querySelector('.btn-reset');
 
 const Name_LS = 'userName';
+
+function handleClickReset() {
+    localStorage.removeItem(Name_LS);
+    userNameInput.value = '';
+}
+
+function resetName() {
+    btnReset.addEventListener('click', function () {
+        handleClickReset();
+        askForUser();
+    });
+}
+
 function saveName(name) {
     localStorage.setItem(Name_LS, name);
 }
 
-function handleSubmit(e) {
+function handleSubmitName(e) {
     e.preventDefault();
     //console.log(userNameInput.value);
     const nameValue = userNameInput.value;
@@ -18,14 +33,15 @@ function handleSubmit(e) {
 }
 
 function paintUser(name) {
-    userNameInput.style.display = 'none';
-    userName.style.display = 'block';
+    userNameContainer.style.display = 'none';
+    userGreeting.style.display = 'block';
     userName.innerText = `Hello ${name}`;
 }
 
 function askForUser() {
+    userGreeting.style.display = 'none';
     userNameContainer.style.display = 'block';
-    userNameContainer.addEventListener('submit', handleSubmit);
+    userNameContainer.addEventListener('submit', handleSubmitName);
 }
 
 function loadName() {
@@ -39,17 +55,18 @@ function loadName() {
 
 function init() {
     loadName();
+    resetName();
 }
 
 init();
 
-//object.querySelector('')
-//localStorage.setItem('KEY','VALUE')
-//localStorate.getItem('KEY')
-//event.preventDefault()
-//object.value
-//object.style.display = ''
-//object.innerText = `STRING ${function}`
-//object.addEventListner('EVENT', function)
-//if...else
-//===
+// object.querySelector('')
+// localStorage.setItem('KEY','VALUE')
+// localStorate.getItem('KEY')
+// event.preventDefault()
+// object.value
+// object.style.display = ''
+// object.innerText = `STRING ${function}`
+// object.addEventListner('EVENT', function)
+// if...else
+// ===
